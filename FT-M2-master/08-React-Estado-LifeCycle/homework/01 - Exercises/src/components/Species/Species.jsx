ea1fc198a9882ef2ce4f5528e19ee32eb0bd1e59
SinthiaGonzalez <128981283+SinthiaGonzalez@users.noreply.github.com>
2023-05-31 02:26:26 +0200

    Delete FT-M2-master directory

--- FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-// import styledSpecies from "./Species.module.css";
-
-export default function Species({ species, handleSpecies, handleAllSpecies }) {
-  // console.log(species);
-  return <div></div>
-}
